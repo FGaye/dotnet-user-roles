313642d3d651d1fa4329087082eb0afa2d8b9255
fg22126029@utg.edu.gm <fatou@writemedia.com>
2023-08-17 15:35:39 +0000

    Added user roles and authentication in extentions

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,35 +1,10 @@
-using System.Text;
-using Api.Data;
-using Api.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-
+using Api.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 {
- builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
- builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:TokenKey"])),
-                ValidateIssuer = false,
-                ValidateAudience = false,
-                ValidateLifetime = true,
-            };
-            });
-            builder.Services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationContext>()
-                .AddDefaultTokenProviders();
-
-    builder.Services.AddScoped<UserManager<User>, UserManager<User>>();
-    builder.Services.AddScoped<SignInManager<User>, SignInManager<User>>();
 
 
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
